c4871194660dcb3cc331c5dab5824305f6ff1b80
Kunakorn Teera-apisukkul <>
2020-02-06 15:48:32 +0700

    add map view.

--- senior-project/components/SelectSports.js
+++ senior-project/components/SelectSports.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {View,Text,StyleSheet,TouchableOpacity, FlatList} from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
-import {withNavigation} from 'react-navigation';
-import {AntDesign} from '@expo/vector-icons';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { withNavigation } from 'react-navigation';
+import { AntDesign } from '@expo/vector-icons';
 
-const SelectSports = ({navigation}) => {
-  return <View style = {styles.backgroundStyle}>
-  <View>
-  <TouchableOpacity onPress = {() => navigation.navigate('Choose')}>
-  <Ionicons name = "ios-football" size={80} style= {styles.iconstyle}/>
-  </TouchableOpacity>
-  </View>
-
-  </View>
+const SelectSports = ({ navigation }) => {
+  return (
+  <View style={styles.backgroundStyle}>
+    <View>
+      <TouchableOpacity onPress={() => navigation.navigate('Choose')}>
+        <Ionicons name="ios-football" size={80} style={styles.iconstyle} />
+      </TouchableOpacity>
+    </View>
+  </View>);
 };
 
 const styles = StyleSheet.create({
-  backgroundStyle:{
+  backgroundStyle: {
     height: 200,
     backgroundColor: '#FFFCF7',
     borderColor: 'black',
@@ -25,11 +25,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginTop: 15
   },
-  iconstyle:{
+  iconstyle: {
     marginLeft: 20,
     fontSize: 100
   },
-  iconstyle2:{
+  iconstyle2: {
     marginLeft: 20,
     fontSize: 80
   }
