05f492b005868fd049e39b89bb2e08ee8aff0b94
Kunakorn Teera-apisukkul <>
2020-02-12 18:00:11 +0700

    change homescreen, response for name

--- senior-project/screens/user/home/HomeScreen.js
+++ senior-project/screens/user/home/HomeScreen.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, Button, AsyncStorage } from 'react-native';
-=======
-import React from 'react';
-import {View,Text,Image,StyleSheet,ScrollView,Button,TouchableOpacity} from 'react-native';
->>>>>>> 2b6a15c210da2c4f08178043ddfff3ab2efbf78d
+import { View, Text, Image, StyleSheet, ScrollView, Button, AsyncStorage, TouchableOpacity } from 'react-native';
 import SelectSports from '../../../components/SelectSports';
 import EventsNearby from '../../../components/EventsNearby';
-import {Ionicons} from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
-<<<<<<< HEAD
 const HomeScreen = ({ navigation }) => {
 
   const [username, setUsername] = useState(null);
@@ -23,50 +17,31 @@ const HomeScreen = ({ navigation }) => {
     fetchUserData();
   }, [username])
 
-  return <View>
-    <ScrollView>
-      <Text style={styles.headText}>  Good morning, {username}  </Text>
-      <Text style={styles.textStyle}> Select the sport that you want to play </Text>
-      <SelectSports />
-      <Text style={styles.headText}>  Events nearby you  </Text>
-      <EventsNearby
-        title="Futsal Park RAMA II"
-        imageSource={require('../../../assets/football.jpg')} />
-      <EventsNearby
-        title="Unun badminton"
-        imageSource={require('../../../assets/badminton.jpg')} />
-      <EventsNearby
-        title="Chris basketball"
-        imageSource={require('../../../assets/basketball.jpg')} />
-    </ScrollView>
-=======
-const HomeScreen = ({navigation}) => {
   return (
     <View>
-  <ScrollView>
-  <Text style= {styles.headText}>  Good morning, Jane Doe!  </Text>
-  <Text style= {styles.textStyle}> Select the sport that you want to play </Text>
-  <View style = {styles.row}>
-  <SelectSports />
-  </View>
-  <Text style= {styles.headText}>  Events nearby you  </Text>
-  <View>
-  <TouchableOpacity onPress = {() => navigation.navigate('Choose')}>
-  <EventsNearby
-   title = "Futsal Park RAMA II"
-   imageSource={require('../../../assets/football.jpg')}/>
-   </TouchableOpacity>
-   </View>
-  <EventsNearby
-  title = "Unun badminton"
-  imageSource={require('../../../assets/badminton.jpg')}/>
-  <EventsNearby
-  title = "Chris basketball"
-  imageSource={require('../../../assets/basketball.jpg')}/>
-  </ScrollView>
->>>>>>> 2b6a15c210da2c4f08178043ddfff3ab2efbf78d
-  </View>
-)
+      <ScrollView>
+        <Text style={styles.headText}>  Good morning, {username}!  </Text>
+        <Text style={styles.textStyle}> Select the sport that you want to play </Text>
+        <View style={styles.row}>
+          <SelectSports />
+        </View>
+        <Text style={styles.headText}>  Events nearby you  </Text>
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate('Choose')}>
+            <EventsNearby
+              title="Futsal Park RAMA II"
+              imageSource={require('../../../assets/football.jpg')} />
+          </TouchableOpacity>
+        </View>
+        <EventsNearby
+          title="Unun badminton"
+          imageSource={require('../../../assets/badminton.jpg')} />
+        <EventsNearby
+          title="Chris basketball"
+          imageSource={require('../../../assets/basketball.jpg')} />
+      </ScrollView>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -80,7 +55,7 @@ const styles = StyleSheet.create({
   textStyle: {
     alignSelf: 'center'
   },
-  row:{
+  row: {
     height: 200,
     backgroundColor: '#FFFCF7',
     borderColor: 'black',
