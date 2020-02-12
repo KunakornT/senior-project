405e778c728c5456a5046966382dc9c334e412b4
Kunakorn Teera-apisukkul <>
2020-02-12 17:49:55 +0700

    add search field

--- senior-project/screens/user/choose/ChooseSports.js
+++ senior-project/screens/user/choose/ChooseSports.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, StyleSheet, Dimensions, TextInput } from 'react-native';
 import MapView from 'react-native-maps';
+import { Ionicons } from '@expo/vector-icons';
+
+import Card from '../../../components/Card';
 
 const ChooseSports = () => {
   return (
     <View style={styles.container}>
+      <Card>
+        <View style={styles.titleContainer}>
+          <Ionicons name='ios-search' size={25} />
+          <TextInput style={styles.text}/>
+        </View>
+      </Card>
       <MapView style={styles.mapStyle} />
     </View>);
 };
@@ -13,12 +22,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  titleContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  search: {
+    height: Dimensions.get('window').height / 10,
+  },
+  text: {
+    fontSize: 20,
+    marginLeft: 5,
+    width: '100%'
   },
   mapStyle: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height/2,
+    height: Dimensions.get('window').height,
   },
 });
 
--- senior-project/screens/user/home/HomeScreen.js
+++ senior-project/screens/user/home/HomeScreen.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import {View,Text,Image,StyleSheet,ScrollView,Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, ScrollView, Button, AsyncStorage } from 'react-native';
 import SelectSports from '../../../components/SelectSports';
 import EventsNearby from '../../../components/EventsNearby';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
+
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      let data = await AsyncStorage.getItem('userInfo');
+      let user = await JSON.parse(data);
+      setUsername(user.username);
+    }
+    fetchUserData();
+  }, [username])
+
   return <View>
-  <ScrollView>
-  <Text style= {styles.headText}>  Good morning, Jane Doe!  </Text>
-  <Text style= {styles.textStyle}> Select the sport that you want to play </Text>
-  <SelectSports />
-  <Text style= {styles.headText}>  Events nearby you  </Text>
-  <EventsNearby
-   title = "Futsal Park RAMA II"
-   imageSource={require('../../../assets/football.jpg')}/>
-  <EventsNearby
-  title = "Unun badminton"
-  imageSource={require('../../../assets/badminton.jpg')}/>
-  <EventsNearby
-  title = "Chris basketball"
-  imageSource={require('../../../assets/basketball.jpg')}/>
-  </ScrollView>
+    <ScrollView>
+      <Text style={styles.headText}>  Good morning, {username}  </Text>
+      <Text style={styles.textStyle}> Select the sport that you want to play </Text>
+      <SelectSports />
+      <Text style={styles.headText}>  Events nearby you  </Text>
+      <EventsNearby
+        title="Futsal Park RAMA II"
+        imageSource={require('../../../assets/football.jpg')} />
+      <EventsNearby
+        title="Unun badminton"
+        imageSource={require('../../../assets/badminton.jpg')} />
+      <EventsNearby
+        title="Chris basketball"
+        imageSource={require('../../../assets/basketball.jpg')} />
+    </ScrollView>
   </View>
 }
 
 const styles = StyleSheet.create({
-  headText:{
+  headText: {
     fontSize: 25,
     fontWeight: 'bold',
     alignSelf: 'center',
     marginTop: 15,
     marginBottom: 10
   },
-  textStyle:{
+  textStyle: {
     alignSelf: 'center'
   },
 });
