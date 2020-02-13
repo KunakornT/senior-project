56c84914bbb56176a0409e357587e979c2652d7f
Kunakorn Teera-apisukkul <>
2020-02-13 15:39:29 +0700

    fix json file

--- senior-project/screens/user/home/HomeScreen.js
+++ senior-project/screens/user/home/HomeScreen.js
@@ -1,16 +1,3 @@
-import React,  { useState, useEffect } from 'react';
-import {View,Text,Image,StyleSheet,ScrollView,Button,TouchableOpacity,AsyncStorage} from 'react-native';
-import SelectSports from '../../../components/SelectSports';
-import EventsNearby from '../../../components/EventsNearby';
-import {Ionicons} from '@expo/vector-icons';
-import url from '../../../constants/url-constant';
-
-const HomeScreen = (props) => {
-  const [username, setUsername] = useState(null)
-
-  useEffect(() => {
-    async function fetchUserData() {
-      let data  = await AsyncStorage.getItem('userInfo');
 import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView, Button, AsyncStorage, TouchableOpacity } from 'react-native';
 import SelectSports from '../../../components/SelectSports';
@@ -28,33 +15,6 @@ const HomeScreen = ({ navigation }) => {
       setUsername(user.username);
     }
     fetchUserData();
-  },[username])
-
-  return (
-    <View>
-  <ScrollView>
-  <Text style= {styles.headText}>  Good morning, {username}!  </Text>
-  <Text style= {styles.textStyle}> Select the sport that you want to play </Text>
-  <View style = {styles.row}>
-  <SelectSports />
-  </View>
-  <Text style= {styles.headText}>  Events nearby you  </Text>
-  <View>
-  <TouchableOpacity onPress = {() => props.navigation.navigate('Choose')}>
-  <EventsNearby
-   title = "Futsal Park RAMA II"
-   imageSource={require('../../../assets/football.jpg')}/>
-   </TouchableOpacity>
-   </View>
-  <EventsNearby
-  title = "Unun badminton"
-  imageSource={require('../../../assets/badminton.jpg')}/>
-  <EventsNearby
-  title = "Chris basketball"
-  imageSource={require('../../../assets/basketball.jpg')}/>
-  </ScrollView>
-  </View>
-)
   }, [username])
 
   return (
