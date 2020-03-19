f1cc8592ac6d4a0f9cedc40ea8ee71b0e8f22cec
Kunakorn Teera-apisukkul <>
2020-03-19 14:42:04 +0700

    event interface

--- senior-project/components/ComingSports.js
+++ senior-project/components/ComingSports.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, Image, ImageBackground, Dimensions } from 'react-native';
+
+import Card from './Card';
 
 const ComingSports = ({ imageSource, title, start, maxPlayer, numberPlayer }) => {
   const startTime = new Date(start).toDateString();
@@ -8,31 +10,52 @@ const ComingSports = ({ imageSource, title, start, maxPlayer, numberPlayer }) =>
     return null;
   }
   return (
-    <View>
-      <Text style={styles.textStyle}>  Sport Field: {title} </Text>
-      <ImageBackground source={imageSource} style={styles.Image}>
-        <Text style={{color: "white", fontSize: 20}}>Date:{startDate}</Text>
-        <Text style={{color: "white", fontSize: 20}}>max:{maxPlayer} player:{numberPlayer}</Text>
-      </ImageBackground>
-    </View>);
+    <Card style={styles.card}>
+      <View>
+        <ImageBackground source={imageSource} style={styles.Image}>
+          <View style={styles.imageContainer}>
+            <Text style={styles.textHeader}>  Sport Field: {title} </Text>
+            <Text style={styles.textStyle}> Date:{startDate}</Text>
+            <Text style={styles.textStyle}> max:{maxPlayer} player:{numberPlayer}</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    </Card>);
 };
 
 const styles = StyleSheet.create({
+  card: {
+    height: Dimensions.get('window').height / 5,
+    width: '90%'
+  },
+  imageContainer: {
+    width: '100%',
+    height: '100%'
+  },
+  textHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    padding: 10,
+  },
   textStyle: {
     fontSize: 18,
-    alignSelf: 'center'
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 5,
   },
   Image: {
-    height: 225,
-    width: 380,
-    resizeMode: 'stretch',
-    borderRadius: 50,
-    marginLeft: 20,
-    marginRight: 20,
-    alignSelf: 'center',
-    borderRadius: 20,
-    marginTop: 15,
-    marginBottom: 15
+    height: '100%',
+    width: '100%',
+    // resizeMode: 'stretch',
+    // borderRadius: 50,
+    // marginLeft: 20,
+    // marginRight: 20,
+    // alignSelf: 'center',
+    // borderRadius: 20,
+    // marginTop: 15,
+    // marginBottom: 15
   }
 });
 
